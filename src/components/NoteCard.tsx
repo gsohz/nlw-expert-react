1a04b9d10535b0dbc15aa524287db949f76b3f61
Gabriel Souza <ga.ferreira.desouza@gmail.com>
2024-02-08 12:40:26 -0300

    Segunda aula: add dialog

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -13,7 +13,7 @@ interface NoteCardProps {
 export function NoteCard({ note }: NoteCardProps) {
   return (
     <Dialog.Root>
-      <Dialog.Trigger className="rounded-md flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 outline-none focus-visible:ring-2 focus-visible:ring-lime-400">
+      <Dialog.Trigger className="rounded-md flex flex-col text-left bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 outline-none focus-visible:ring-2 focus-visible:ring-lime-400">
         <span className="text-sm font-medium text-slate-300">
           {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
         </span>
