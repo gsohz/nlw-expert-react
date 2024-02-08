87ee507ce41f29d3df13f612d5b66c8a084b72a4
Gabriel Souza <ga.ferreira.desouza@gmail.com>
2024-02-08 17:18:47 -0300

    Terceira aula: add reset text on dialog close

--- src/components/NewNoteCard.tsx
+++ src/components/NewNoteCard.tsx
@@ -88,6 +88,16 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
     }
   }
 
+  function handleCloseDialog() {
+    setIsRecording(false)
+    if (speechRecognition !== null) {
+      speechRecognition.stop()
+    }
+
+    setContent('')
+    setShouldShowOnboarding(true)
+  }
+
   return (
     <Dialog.Root>
       <Dialog.Trigger className="rounded-md flex flex-col bg-slate-700 p-5 gap-3 text-left hover:ring-2 hover:ring-slate-600 outline-none focus-visible:ring-2 focus-visible:ring-lime-400">
@@ -103,7 +113,10 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/60" />
         <Dialog.Content className="fixed inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none overflow-hidden">
-          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+          <Dialog.Close
+            onClick={handleCloseDialog}
+            className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100"
+          >
             <X className="size-5" />
           </Dialog.Close>
 
